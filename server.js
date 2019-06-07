e9a909db30de443e1a0bf3cf35f46376594f1fb4
Minh Phan <minhphan2603@gmail.com>
2019-06-07 19:27:26 +0700

    change something

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-// const courses = require("./model/courses");
+const courses = require("./model/courses");
 
-const fakeCourse = require('./model/fakecourse');
+// const fakeCourse = require('./model/fakecourse');
 
 mongoose
   .connect("mongodb://localhost:27017/data", { useNewUrlParser: true })
@@ -9,17 +9,26 @@ mongoose
   .catch(err => console.log(err));
 
 // newCourse = new courses({
-//   name: "Minh",
-//   age: 26
+//   	name: "FullStack",
+//     author: "Minh",
+//     tag: ["NodeJS","ReacrJS"],
+//     isPublished: true,
+//     price: 300
 // });
 
 // newCourse.save()
 // 	.then(course => console.log(course));
 
-fake = new fakeCourse({
-	name: "Nhat Minh"
-})
+// fake = new fakeCourse({
+// 	name: "Nhat Minh"
+// })
 
-fake.save()
-	.then(res => console.log(res))
-	.catch(err => console.log(err));
+// fake.save()
+// 	.then(res => console.log(res))
+// 	.catch(err => console.log(err));
+
+courses.findById('5cf8eae7254cb23a508fd08c')
+	.then(course => {
+		course.price = 200;
+		course.save().then(c => console.log(c))
+	});
